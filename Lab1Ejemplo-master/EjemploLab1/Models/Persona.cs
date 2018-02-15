dae2b0b9e4014664a5b6f724ac6b3c0d5f58eb1a
renatocabreraf <renatojose98@yahoo.com>
2018-02-15 08:26:37 -0600

    PARAMETROS

--- Lab1Ejemplo-master/EjemploLab1/Models/Persona.cs
+++ Lab1Ejemplo-master/EjemploLab1/Models/Persona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,27 @@ namespace EjemploLab1.Models
 {
     public class Persona
     {
+        [Key]
         public int PersonaID { get; set; }
+
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Se necesita un Club")]
 
         public string Club { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita un Apellido")]
+
         public string Apellido { get; set; }
-        public int Edad { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita un Nombre")]
+
         public string Nombre { get; set; }
+
+        [Display (Name = "Posici[on dentro del campo")]
+
         public string Posicion { get; set; }
+
+        [Range (1000,10000000000)]
+
         public double Salario { get; set; }
     }
 }
