e75adbc5b84cbd59d60ba99fecfb64e4925908cc
renatocabreraf <renatojose98@yahoo.com>
2018-02-18 17:35:24 -0600

    ACTUALIZADO

--- Lab1Ejemplo-master/EjemploLab1/Models/Persona.cs
+++ Lab1Ejemplo-master/EjemploLab1/Models/Persona.cs
@@ -8,20 +8,19 @@ namespace EjemploLab1.Models
 {
     public class Persona
     {
-        [Key]
+       
         public int PersonaID { get; set; }
 
 
-        [Required (AllowEmptyStrings = false, ErrorMessage = "Se necesita un Club")]
+   
 
         public string Club { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita un Apellido")]
+        
 
         public string Apellido { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Se necesita un Nombre")]
-
+     
         public string Nombre { get; set; }
 
        
